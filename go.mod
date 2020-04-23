043c412963c53cce0303077358a55ea905a47f5c
Kyungtae Lee <mlogue76@gmail.com>
2020-04-23 18:04:20 +0900

    gin

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module aergohub/hellopod
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.6.2
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	n, _ := os.Hostname()
-	fmt.Printf("Hello Pod `%s`\n", n)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		n, _ := os.Hostname()
+		c.String(http.StatusOK, "Hello World from `%s`", n)
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
